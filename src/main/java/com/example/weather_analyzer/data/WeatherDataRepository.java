841a6ebccf52533aabb6d895c712c5507f63a4df
Andrei Pronsky <andrey.pronsky@gmail.com>
2023-12-01 22:34:05 +0300

    Data layer performed

--- src/main/java/com/example/weather_analyzer/data/WeatherDataRepository.java
+++ src/main/java/com/example/weather_analyzer/data/WeatherDataRepository.java
@@ -1,2 +1,13 @@
-package com.example.weather_analyzer.data.entities;public interface WeatherDataRepository {
+package com.example.weather_analyzer.data;
+
+import com.example.weather_analyzer.data.entities.WeatherData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
+    @Query("FROM WeatherData wd WHERE wd.id BETWEEN :start_period AND :end_period")
+    List<WeatherData> findAllIdBetween(@Param("start_period") Long startPeriod, @Param("end_period") Long endPeriod);
 }
---
+++ src/main/java/com/example/weather_analyzer/data/entities/WeatherData.java
@@ -0,0 +1,64 @@
+package com.example.weather_analyzer.data.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "weather_data")
+public class WeatherData {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "temperature")
+    private Double temperature;
+
+    @Column(name = "wind_speed")
+    private Double windSpeed;
+
+    @Column(name = "pressure")
+    private Double pressure;
+
+    @Column(name = "humidity")
+    private Integer humidity;
+
+    @Column(name = "conditions")
+    private String conditions;
+
+    @Column(name = "location")
+    private String location;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeatherData that = (WeatherData) o;
+        return Objects.equals(id, that.id) && Objects.equals(temperature, that.temperature) && Objects.equals(windSpeed, that.windSpeed) && Objects.equals(pressure, that.pressure) && Objects.equals(humidity, that.humidity) && Objects.equals(conditions, that.conditions) && Objects.equals(location, that.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, temperature, windSpeed, pressure, humidity, conditions, location);
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "id=" + id +
+                ", temperature=" + temperature +
+                ", wind_speed=" + windSpeed +
+                ", pressure=" + pressure +
+                ", humidity=" + humidity +
+                ", conditions='" + conditions + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
